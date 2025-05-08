ef98f379efee4472c70e3886af6881197458b02a
maximus676 <grillax.minsk@gmail.com>
2025-05-08 13:21:21 +0300

    data type

--- glava_1/yrok_1.go
+++ glava_1/yrok_1.go
@@ -1,10 +1,24 @@
-package main // определение пакета для текущего файла
+package main
 
 import "fmt"
 
 func main() {
+	var hello string = "start"
+	hello = "Hello world"
+	text := "Max"
+	var tab string = "\tТом \nСой\\ер"
 
-	fmt.Println("Hello Go")
-	fmt.Println("Hello Go2")
+	var age uint8
+	fmt.Println(tab)
+	fmt.Println(hello)
+	fmt.Println(len(text))
+
+	var name string
+	fmt.Println("wfat is your name?")
+	fmt.Scan(&name)
+	fmt.Println("Hello " + name + " !")
+	fmt.Println("How old are you ?")
+	fmt.Scan(&age)
+	fmt.Println("you are " + fmt.Sprint(age) + " yers !") //число со строкой выводить нельзя по этому используем Sprint
+
 }
-
